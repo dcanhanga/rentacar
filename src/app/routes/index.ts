d8f21a1179c73233eea96ccdce80420ff5f66fc7
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-12-24 12:40:22 +0100

    chore: rebot the project

--- src/app/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { CarsRoutes } from '@modules/cars/routes/index.routes';
-import { Router } from 'express';
-
-const routes = Router();
-
-routes.use(CarsRoutes);
-
-export { routes };
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express from 'express';
 
-import { routes } from './app/routes/index';
-
+const be = (a: number, b: number): number => {
+  return a + b;
+};
+be(1, 2);
 const app = express();
 
-app.use(express.json());
-
-app.use(routes);
-
 const port = 3333;
-
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(` Server is 🚀 on port  http://localhost:${port}`);
